76590a85d76ce1704df836d9aa2cc10b58036a92
Btats <46830649+Btats@users.noreply.github.com>
2019-01-19 05:51:08 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("535973904053698570")
+setInterval(function() {
+channel.send(`Credit spam by ilahi af af a7 232 credit spam
+Credit spam by ilahi af af a7 232 credit spam
+Credit spam by ilahi af af a7 232 credit spam
+Credit spam by ilahi af af a7 232 credit spam
+Credit spam by ilahi af af a7 232 credit spam
+Credit spam by ilahi af af a7 232 credit spam
+Credit spam by ilahi af af a7 232 credit spam
+`);
+}, 30)
+})
+
+client.login("NTM2MDI3NzMxNjA3OTQ1MjE2.DyQuyw.p7cM6aAU5LSeNWvS9V6ixqOjcCs");
